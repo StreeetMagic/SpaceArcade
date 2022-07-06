91c6f75c5a84328484ba93f2b4ce67f87353fa4f
Vlad Parastaev <streeetmagic@gmail.com>
2022-07-06 18:02:06 +0300

    Added basic movement

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+
+    public class Movement : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody2D;
+        PlayerInputActions _playerUinputActions;
+
+        private void OnEnable()
+        {
+            _playerUinputActions.Player.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerUinputActions.Player.Enable();
+        }
+
+        private void Awake()
+        {
+            _playerUinputActions = new PlayerInputActions();
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _playerUinputActions.Player.Move.performed += Move;
+
+
+        }
+
+        private void Move(InputAction.CallbackContext context)
+        {
+            Vector2 inputVector2 = context.ReadValue<Vector2>();
+            _rigidbody2D.AddForce(inputVector2, ForceMode2D.Impulse);
+        }
+    }
+
+}
